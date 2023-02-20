6a635454adbb52b29ff22b490a817afae1d1c4a6
chenjason21 <57476440+chenjason21@users.noreply.github.com>
2023-02-20 20:36:19 +0700

    Delete source directory

--- source/variabel.py
+++
@@ -1,5 +0,0 @@
-
-UserNameHRM=  'Admin'   #Scalar
-PasswordHRM=  'admin123'    #Scalar
-CredentialsTheInternetHerokuApp=  'tomsmith' SuperSecretPassword!  #List
-VisibleElements=  'OrangeHRM=id:welcome'  InternetHerokuApp=css:[href="/logout"]  #Dictionary
